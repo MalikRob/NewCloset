bb3c45e5786a01e35919c4de265818eaaed2db74
Malik Robinson <malikrob263@yahoo.com>
2022-04-13 19:21:14 -0400

    New Button Leads To Home

--- app/src/main/java/com/example/allin/fragments/list/altListFragment.kt
+++ app/src/main/java/com/example/allin/fragments/list/altListFragment.kt
@@ -1,19 +1,76 @@
 package com.example.allin.fragments.list
 
+import android.app.AlertDialog
 import android.os.Bundle
+import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.allin.R
+import com.example.allin.viewmodel.ClothingViewModel
+import kotlinx.android.synthetic.main.fragment_alt_list.view.*
 
 class altListFragment : Fragment() {
+
+    private lateinit var mClothingViewModel: ClothingViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_alt_list, container, false)
+        val view = inflater.inflate(R.layout.fragment_alt_list, container, false)
+
+        //RecyclerView
+        val adapter = ListAdapter()
+        val recyclerView = view.recyclerviewO
+        recyclerView.adapter = adapter
+        //recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        recyclerView.layoutManager = GridLayoutManager(requireContext(), 2)
+
+        //ClothingViewModel
+        mClothingViewModel = ViewModelProvider(this).get(ClothingViewModel::class.java)
+        mClothingViewModel.readAllData.observe(viewLifecycleOwner, Observer { clothing ->
+            adapter.setData(clothing)
+        })
+
+        //Arrow Button now takes the user back to the Home screen
+        view.homeButton.setOnClickListener{
+            findNavController().navigate(R.id.action_altListFragment_to_listFragment)
+        }
+
+        //Add menu
+        setHasOptionsMenu(true)
+
+        return view
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.delete_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.menu_delete){
+            deleteAllUsers()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun deleteAllUsers() {
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setPositiveButton("Yes") { _, _ ->
+            mClothingViewModel.deleteAllClothing()
+            Toast.makeText(
+                requireContext(),
+                "Successfully removed everything.",
+                Toast.LENGTH_SHORT).show()
+        }
+        builder.setNegativeButton("No") { _, _ ->}
+        builder.setTitle("Delete everything?")
+        builder.setMessage("Are you sure you want to delete everything?")
+        builder.create().show()
     }
 }
